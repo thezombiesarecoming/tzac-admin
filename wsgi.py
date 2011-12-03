17a1c561bc47b0aa8b4336d99686d9ba76a068c0
Travis Swicegood <development@domain51.com>
2011-12-03 17:07:18 -0600

    add ability to override settings with env var

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,8 @@ import sys
 
 sys.path.insert(0, join(dirname(__file__), "website"))
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'website.settings.development'
+if "DJANGO_SETTINGS_MODULE" not in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'website.settings.development'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
