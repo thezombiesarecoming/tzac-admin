54b4c958da872496b9834c0140e932d00878b90b
APBurke <andrewpeterburke@gmail.com>
2011-12-04 13:04:20 -0600

    adds subscribers forms

---
+++ website/tzac_subscribers/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class SubscriberForm(forms.Form):
+    '''Subscription form for signing up for notifications
+
+    all fields are independent'''
+    phone_number = forms.IntegerField(required=False)
+    twitter = forms.CharField(max_length=200, required=False)
+    email = forms.EmailField(required=False)
