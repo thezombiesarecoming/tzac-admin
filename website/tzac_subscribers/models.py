d738c20b2c74a24979a13050eece93effc9e5456
Travis Swicegood <development@domain51.com>
2011-12-04 14:06:57 -0600

    add a unicode method for subscribers

--- website/tzac_subscribers/models.py
+++ website/tzac_subscribers/models.py
@@ -5,3 +5,6 @@ class Subscriber(models.Model):
     '''Represents a service subscriber to be notified'''
     type = models.CharField(max_length=200)
     value = models.TextField()
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.value, self.type)
