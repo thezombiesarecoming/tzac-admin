a5e34dbbb54cf48ac6f50578010dbe3d121ea3f2
Travis Swicegood <development@domain51.com>
2011-12-04 09:33:42 -0600

    Swap out to use non-Armstrong like settings in wsgi file

--- wsgi.py
+++ wsgi.py
@@ -5,9 +5,7 @@ import sys
 sys.path.insert(0, join(dirname(__file__), "website"))
 
 if "DJANGO_SETTINGS_MODULE" not in os.environ:
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'website.settings.development'
-
-print os.environ["DJANGO_SETTINGS_MODULE"]
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'website.settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
